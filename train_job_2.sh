4a6af429d4511b19c4c87380c3b5a1540543082f
ulgrl@student.kit.edu <ka_ulgrl@uc3n990.localdomain>
2025-07-23 21:55:36 +0200

    3 tasks

--- train_job_2.sh
+++ train_job_2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=gr00t_offline_train
+#SBATCH --job-name=task_1
 #SBATCH --partition=gpu_h100
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=20G
-#SBATCH -t 10:00:00
+#SBATCH -t 6:00:00
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --error=logs/%x-%j.err
+#SBATCH --mail-type=BEGIN,END,FAIL    
+#SBATCH --mail-user=ulgrl@student.kit.edu
+
 
 source /home/ka/ka_stud/ka_ulgrl/miniconda3/etc/profile.d/conda.sh
 conda activate /home/ka/ka_stud/ka_ulgrl/miniconda3/envs/gr00t
